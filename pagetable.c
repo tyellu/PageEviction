888e25683dc7abddc83b7676246267cee4ac12a3
Nick G <gptt916@hotmail.com>
2017-02-24 17:45:45 -0500

    added bit shifting in find_physpage

--- pagetable.c
+++ pagetable.c
@@ -188,7 +188,7 @@ char *find_physpage(addr_t vaddr, char type) {
 			init_frame(new_frame, vaddr);
 
 			// Do we need to bitshift here?
-			// p->frame = frame << PAGE_SHIFT;
+			p->frame = new_frame << PAGE_SHIFT;
 
 			// set it to dirty
 			p->frame |= PG_DIRTY;
@@ -201,7 +201,7 @@ char *find_physpage(addr_t vaddr, char type) {
 			swap_pagein(new_frame, p->swap_off);
 
 			// bit shifting left by PAGE_SHIFT
-			p->frame = frame << PAGE_SHIFT;
+			p->frame = new_frame << PAGE_SHIFT;
 
 			// Mark it as not dirty
 			p->frame &= ~PG_DIRTY;
